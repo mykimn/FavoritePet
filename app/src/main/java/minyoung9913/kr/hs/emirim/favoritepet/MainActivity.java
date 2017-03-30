20b2227b7e0a13a7b53b73b1103ab630a599f9ce
mykimn <min0905@e-mirim.hs.kr>
2017-03-30 20:57:16 +0900

    #4 Complete Button Event PG
    git add .

--- app/src/main/java/minyoung9913/kr/hs/emirim/favoritepet/MainActivity.java
+++ app/src/main/java/minyoung9913/kr/hs/emirim/favoritepet/MainActivity.java
@@ -10,9 +10,10 @@ import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener{
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener,View.OnClickListener{
     CheckBox checkSelect;
     RadioGroup rg;
     TextView textQuest;
@@ -33,8 +34,8 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         butOk = (Button)findViewById(R.id.but_ok);
         imgvPet = (ImageView)findViewById(R.id.imgv_pet);
         checkSelect.setOnCheckedChangeListener(this);
-
-    }
+        butOk.setOnClickListener(this);
+    } // end onCreate
 
     @Override
     public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
@@ -49,6 +50,22 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
             rg.setVisibility(View.INVISIBLE);
             butOk.setVisibility(View.INVISIBLE);
             imgvPet.setVisibility(View.INVISIBLE);
+        }
+    }
+    @Override
+    public void onClick(View view){
+        switch(rg.getCheckedRadioButtonId()){
+            case R.id.radio_dog:
+                imgvPet.setImageResource(R.drawable.dog);
+                break;
+            case R.id.radio_hamster:
+                imgvPet.setImageResource(R.drawable.hamster);
+                break;
+            case R.id.radio_rabbit:
+                imgvPet.setImageResource(R.drawable.rabbit);
+                break;
+            default:
+                Toast.makeText(this, "라디오버튼이 선택이 안되었군요.",Toast.LENGTH_SHORT).show();
         }
     }
 }
